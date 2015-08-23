22f893fe9fa64cf6846376e87400d00c536f57b3
Michel Perez <michel.ingesoft@gmail.com>
2015-08-22 22:49:15 -0500

    delete project

---
+++ test/project_delete_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"os"
+
+	"bitbucket.org/kiloops/toolbelt/command"
+	"github.com/gosimple/slug"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ProjectDelete", func() {
+
+	BeforeEach(func() {
+		cleanDB()
+		command.CreateAccount(&userLogin, SSHPath)
+		command.Login(&userLogin)
+		command.ProjectCreate(&project)
+	})
+
+	AfterEach(func() {
+		name := slug.Make(project.Name)
+		os.RemoveAll(name)
+	})
+
+	It("deletes an existing project", func() {
+		err := command.ProjectDelete(project.Slug)
+		Expect(err).To(BeNil())
+	})
+
+	It("deletes an unexisting project so should fail", func() {
+		err := command.ProjectDelete("demo-un")
+		Expect(err).To(BeEquivalentTo(command.ErrProjectNotFound))
+	})
+
+})
